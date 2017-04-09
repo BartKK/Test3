7ef42359614ff42340dde2ea1763d3b7b94f4493
BartKK <Bartkukulka@gmail.com>
2017-04-09 11:27:14 +0200

    Copy repository

---
+++ app.js
@@ -0,0 +1,71 @@
+'use strict';
+//http://api.openweathermap.org/data/2.5/forecast/daily?q=Gdansk&cnt=2&appid=1e60284c59368f64086bca6970e8375a
+var weatherApp = angular.module('weatherApp',[
+  'ngRoute',
+  'ngResource'
+
+]);
+
+
+// CONFIG
+weatherApp.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: '/views/home.html',
+            controller: 'homeController'
+        })
+        .when('/forecast', {
+          templateUrl: '/views/forecast.html',
+          controller: 'forecastController'
+        })
+}]);
+
+// SERVICE
+weatherApp.service('cityService', function () {
+  this.city = 'Gdansk';
+});
+// CONTROLLERS
+weatherApp.controller('homeController', ['$scope', 'cityService',
+
+ function ($scope, cityService) {
+   $scope.city = cityService.city;
+
+  $scope.$watch('city', function () {
+    cityService.city = $scope.city;
+  })
+}]);
+
+weatherApp.controller('forecastController', ['$scope', '$resource', 'cityService',
+ function ($scope, $resource, cityService) {
+    var weatherApi = $resource('http://api.openweathermap.org/data/2.5/forecast/daily');
+    $scope.weatherResult = weatherApi.get({
+      q: 'Gdansk',
+      cnt: 2,
+      appid: '1e60284c59368f64086bca6970e8375a'
+}, function (res) {
+  console.log(res);
+  return res;
+
+    });
+
+//     .$promise.then(function (data) {
+// console.log(data)
+//       return data;
+//     }).catch(function (err) {
+//       console.log(err)
+//     });
+
+
+   $scope.city = cityService.city;
+
+$scope.convertToCelsius = function (data) {
+  if (data < (0)) {
+		return 'below absolute zero (0 K)';
+	} else {
+		return (data-273.15).toFixed(1) + ('°C');
+	}
+}
+
+//asdasd
+
+}]);
